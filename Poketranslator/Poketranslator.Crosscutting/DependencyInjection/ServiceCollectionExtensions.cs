e166290a79505ab7ebe3bc495c295c560167fdd8
Alexandre Fernandes <mteknight@hotmail.com>
2020-02-26 00:31:14 +0000

    extended crosscutting with dependency injection configuration for data services

---
+++ Poketranslator/Poketranslator.Crosscutting/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Poketranslator.Data.External.PokemonApi;
+using Poketranslator.Data.Interfaces.External.PokemonApi;
+
+namespace Poketranslator.Crosscutting.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureDataDependencies(this IServiceCollection services)
+        {
+            return services
+                .AddSingleton<IPokemonApiService, PokemonApiService>();
+        }
+    }
+}
