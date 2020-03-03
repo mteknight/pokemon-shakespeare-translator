5fa554c67ebe8f5043ac7f42fccb202922ad4e24
Alexandre Fernandes <mteknight@hotmail.com>
2020-03-03 23:14:45 +0000

    wired injection into the api

--- Poketranslator/Poketranslator.API/Startup.cs
+++ Poketranslator/Poketranslator.API/Startup.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Poketranslator.Crosscutting.DependencyInjection;
 
 namespace Poketranslator.API
 {
@@ -24,7 +19,10 @@ namespace Poketranslator.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services
+                .ConfigureDataDependencies()
+                .ConfigureDomainDependencies()
+                .AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
